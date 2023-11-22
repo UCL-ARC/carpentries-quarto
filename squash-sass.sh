92621bd58b639b44ef2835caec40904ff224c97b
Milan Malfait <m.malfait@ucl.ac.uk>
2023-11-22 15:27:22 +0000

    Add icon copying to squash script

--- squash-sass.sh
+++ squash-sass.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 out=_extensions/carpentries/assets
-mkdir -p ${out}/{fonts,images}
+mkdir -p ${out}/{fonts,images,icons}
 cp -r source/fonts/* ${out}/fonts/
 cp -r source/images/* ${out}/images/
+cp -r source/icons/* ${out}/icons/
 
 node_modules/.bin/sass -s compressed \
   source/stylesheets/styles.css.scss \
