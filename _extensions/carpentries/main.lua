9d381d4b82368812c52dbf4569dcdc4f6d2da188
Milan Malfait <m.malfait@ucl.ac.uk>
2023-11-23 11:04:51 +0000

    Add head HTML template with Lua

--- _extensions/carpentries/main.lua
+++ _extensions/carpentries/main.lua
@@ -1,16 +1,18 @@
+function setCarpentriesStyling(meta)
+    -- Add general styling from CSS and JavaScript files
+    quarto.doc.add_html_dependency({
+        name = "carpentries-style",
+        scripts = { "assets/scripts.js" },
+        stylesheets = { "assets/styles.css" }
+    })
+
+    -- Include the html templates
+    quarto.doc.include_file('in-header', 'templates/head.html')
+
+end
+
 return {
     {
-        Meta = function(meta)
-            quarto.doc.add_html_dependency({
-                name = "carpentries-lesson",
-                scripts = {"assets/scripts.js"},
-                stylesheets = {"assets/styles.css"}
-            })
-        end
-    }, {
-        -- Header = function(el)
-        --     quarto.log.output("=== Handling Header ===")
-        --     quarto.log.output(el)
-        -- end
-    }
+        Meta = setCarpentriesStyling
+    },
 }
